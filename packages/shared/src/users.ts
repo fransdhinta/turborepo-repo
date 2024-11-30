352dd7a5463c6d38ab4ddce597ec73f5731474ee
fransdhinta <dhintaF@gmail.com>
2024-11-30 22:35:01 +0700

    fix

--- packages/shared/src/users.ts
+++ packages/shared/src/users.ts
@@ -1,7 +1,7 @@
 export interface User {
-    id: string;
+    id?: string;
     name: string;
     address: string;
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
